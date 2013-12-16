0fb3d39c13f64605f6067e887531bff4151901a0
Michael Halling <michael.halling@visma.com>
2013-12-16 21:38:54 +0100

    misc work

--- app/js/TimeTerminalMain.js
+++ app/js/TimeTerminalMain.js
@@ -1,19 +1,61 @@
 var timeTerminal = angular.module("timeTerminal", []);
 
+var Action = function(id, description)
+{
+	this.id = id;
+	this.description = description;
+};
+
+
 timeTerminal.factory("Layout", function() {
 	//return {message: "data from service!"}
 	return { rows: [ ["1","2","3"],
 					["4","5","6"],
 					["7","8","9"],
-					["<","0","!"]]
+					["<","0","!"]],
+			 actionButtons : [	new Action(0, 'In'), 
+    							new Action(1, 'out lunch'),
+    							new Action(2, 'in lunch'),
+    							new Action(3, 'Out'), 
+							 ]
 				};
 });
 
 timeTerminal.factory("Data", function() {
-	return { personId: "",
-			 addToPersonId: function(data) {
-			 	this.personId = this.personId + data;
-			 }}
+	return { 
+			personId: "",
+			 buttonPress: function(newValue) {
+	 			this.addValue(newValue);
+			 },
+			 addValue: function(newValue) {
+			 	if (newValue=='<')
+					{
+						this.removeLastValue();
+						return;
+					}
+				if (newValue=="!")
+				{
+					this.clearValues();
+					return;
+				}
+			 	this.personId = this.personId + newValue;
+			 },
+			 clearValues: function() {
+			 	this.personId='';
+			 },
+			 removeLastValue : function() {
+			 	if (this.personId.length > 0)
+				{
+					var actualValues = this.personId;
+					var newValues = actualValues.substring(0, actualValues.length-1);
+					this.personId = newValues;
+				}
+			 },
+			 actionButtonPress : function(actionButton) {
+			 	alert("hej!");
+			 }
+
+			}
 })
 
 
--- scripts/test.bat
+++ scripts/test.bat
@@ -8,4 +8,5 @@ REM - NodeJS (http://nodejs.org/)
 REM - Karma (npm install -g karma)
 
 set BASE_DIR=%~dp0
+set CHROME_BIN=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
 karma start "%BASE_DIR%\..\config\karma.conf.js" %*
