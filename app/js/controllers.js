77d449b6f4b1047aba6ac3b97d11a7356fef8e79
Wholing <thewholing@gmail.com>
2014-02-05 01:06:25 +0100

    Routing implemented

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,124 @@
 'use strict';
 
 /* Controllers */
-
+	var Person = function(id, name)
+		{
+			return {"id" : id, "name" : name, "actions": []};
+		};
+  		
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ['$scope','$timeout', 'MemoryData', function($scope, $timeout, MemoryData) {
+  		
+
+  		$scope.data = MemoryData;
+		//$scope.data.add(1, new Person(1,"Nils Nilsson"));
+		$scope.data.add(1, {"id" : 1, "name" : "Nils Nilsson", "actions": []});
+		$scope.data.save();
+
+	  	$scope.layout = { rows: [ ["1","2","3"],
+						["4","5","6"],
+						["7","8","9"],
+						["<","0","!"]],
+				 actionButtons : [ 	{"id": 0, "description": "In"},
+				 					{"id": 1, "description": "out lunch"},
+				 					{"id": 2, "description": "in lunch"},
+				 					{"id": 3, "description": "Out"}
+								 ]
+					};
+
+
+
+	$scope.personId = "";
+	$scope.message = {
+		text : "some information",
+		textClass : "info",
+		visible : true,
+		displayTime : 2000
+	} 
+
+ 	$scope.buttonPress = function(newValue) {
+		$scope.addValue(newValue);
+	};
+
+	$scope.addValue = function(newValue) {
+	 	if (newValue=='<')
+			{
+				this.removeLastValue();
+				return;
+			}
+		if (newValue=="!")
+		{
+			$scope.clearValues();
+			return;
+		}
+	 	this.personId = this.personId + newValue;
+	}
+	
+	$scope.clearValues = function() {
+		$scope.personId='';
+ 	};
+			 
+	 $scope.removeLastValue = function() {
+	 	if ($scope.personId.length > 0)
+		{
+			var actualValues = $scope.personId;
+			var newValues = actualValues.substring(0, actualValues.length-1);
+			$scope.personId = newValues;
+		}
+	 };
+
+
+	$scope.actionButtonPress = function(actionButton) {
+	 	$scope.addAction(actionButton);
+	};
+
+	$scope._getPerson = function() {
+	    	return MemoryData.getById($scope.personId);
+	};
+
+	$scope.addAction = function(action) {
+		var person = this._getPerson();
+		if (person == null)
+		{
+		    $scope.showErrorMessage("Person not found");
+		}
+		else
+		{
+			person.actions.push(action);
+			MemoryData.add(person);
+			MemoryData.save();
+			$scope.showInfoMessage("Data saved.")
+			//self.actions.push(action);
+		}
+		$scope.clearValues();
+	};
+
+	$scope.showErrorMessage = function (text) {
+		$scope.showMessage(text, "alert alert-danger");
+	};
+
+	$scope.showInfoMessage = function (text) {
+		$scope.showMessage(text, "alert alert-success");
+	};
+
+	$scope.showMessage = function (text, textclass) {
+		$scope.message.text = text;
+		$scope.message.textClass = textclass;	
+		$scope.message.visible = true;	
+		$timeout($scope.clearMessage, $scope.message.displayTime);
+	};
+
+
+	$scope.clearMessage = function () {		
+		$scope.message.visible = false;
+	}	
+
+
+
+
 
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MyCtrl2', ['$scope','$timeout', 'MemoryData', function($scope, $timeout, MemoryData) {
+  	$scope.data = MemoryData.getAll();
+  	$scope.text = "Hej!"
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,36 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
+var myServices = angular.module('myApp.services', []).
   value('version', '0.1');
+
+myServices.factory("MemoryData", function() {
+	return {
+				self : this,
+				_pendingdata : [],
+				_data : [],
+				
+				add : function(id, data){
+					var toAdd = { 'id': id, 'data' : data};
+					this._pendingdata.push(toAdd);
+				},
+
+				save : function(){
+					this._data.push.apply(this._data, this._pendingdata);
+					this._pendingdata = [];
+				},
+
+				getById : function(id){
+					for (var i = this._data.length - 1; i >= 0; i--) {
+						if (this._data[i].id==id)
+						return this._data[i].data;
+					}; 
+					return null;
+				},
+
+				getAll : function() {
+					return this._data;
+				}			
+
+	}
+});
